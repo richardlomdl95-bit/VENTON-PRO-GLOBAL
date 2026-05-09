eecf2cdd00dcaa9a6aaff1d7910e388f67bb16ba
richardlomdl95-bit <richardlomdl95@gmail.com>
2026-05-09 14:16:03 -0500

    fix: Add hide Path to latlong2 import to resolve type conflict

--- lib/pages/turismo_mapa_page.dart
+++ lib/pages/turismo_mapa_page.dart
@@ -11,7 +11,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:latlong2/latlong.dart' hide Path;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:url_launcher/url_launcher.dart';
